032eb0bb3f67dbd3ada7444f7edd9ea50af42bff
Brent <b.dodewaard@gmail.com>
2020-09-05 21:04:03 +0200

    added dimac loading and completed CNF structure, added log section in readme"

--- README.md
+++ README.md
@@ -3,12 +3,6 @@ We need to remember to document: the important design decisions for your SAT sol
 
 ## TODO
 #### Now (ish)
-- Read dimac and construct CNF structure
-- Translating sudoku into a CNF readable form  
-      Check code file SudToCNF.py for an implementation. Command line : python SudToCNF.py Path\To\SudokuFile.txt
-      If you want to test a file easily, put it in the same folder as the code. Don't forget the ".txt" at the end of your file.
-      I've tested it on 4x4.txt, 16x16.txt and damnhard.sdk.txt. Feel free to ask questions. - Jade
-
 - Basic SAT Solver <- divide into sub-task!!
 - Recursive vs iterative (Jade!?!?)  
       If we can do one, we can do the other. Final decision should probably be made based on our experiment. - Jade
@@ -21,6 +15,13 @@ We need to remember to document: the important design decisions for your SAT sol
 - Your experimental results (consider including plots or graphs or bar charts, and to test for statistical significance of your results)
 - The conclusion about your hypothesis that you draw from your results (and why)
 
+# Log
+- Jade: Translating sudoku into a CNF readable form  
+    Check code file SudToCNF.py for an implementation. Command line : python SudToCNF.py Path\To\SudokuFile.txt
+    If you want to test a file easily, put it in the same folder as the code. Don't forget the ".txt" at the end of your file.
+    I've tested it on 4x4.txt, 16x16.txt and damnhard.sdk.txt. Feel free to ask questions. - Jade
 
+- Brent:  Read dimac and construct CNF structure.
+    I got dimac loading working! I get a different amount of variables and clauses then the file says though, but someone else on piazza had the same problem and the same count as me, so the file might actually be wrong. Doesnt work with the SudToCNF script yet as it only accepts files right now. Should change it to also be able to accept strings instead of just files so SudToCNF can be an input.
 
 
--- code/classes/CNF.py
+++ code/classes/CNF.py
@@ -3,42 +3,103 @@
 class Variable():
     """Represents a variable"""
 
-    def __init__(self):
+    def __init__(self, name):
         # Name of the variable (e.g. 112)
-        self.variable_name =
+        self.variable_name = name
 
         # Current boolean status of the variable, starts as None
-        self.boolean = 
+        self.boolean = None
 
 class Literal():
     """Represents a single literal"""
 
-    def __init__(self, ):
+    def __init__(self, variable, negated):
         # The variable "name" this literal represents
-        self.variable = 
+        self.variable = variable
 
-        # Bool which represents if the literal is negated or not.
-        self.negation = 
+        # Bool which represents if the literal is negated or not
+        self.negation = negated
+    
+    def __str__(self):
+        return ("-" * self.negation) + (self.variable.variable_name)
 
 class Clause():
     """"Represents a single clause, which is a disjunction of literals"""
 
-    def __init__(self,):
+    def __init__(self, literals):
 
         # List of literals that are contained in the Clause.
-        self.literals = 
+        self.literals = literals
+    
+    def __str__(self):
+        string = ""
+        for literal in self.literals:
+            string += "{} v ".format(literal)
+        return string[:-3]
         
+    
 class CNF_Formula():
     """Represents a CNF_Formula, which is a conjuction of clauses"""
 
-    def __init__(self, clause_list):
+    def __init__(self):
         # Dict with variables thats are used to buid literal and thus clauses
-        self.variable_dict = {"variable name": Variable()}
+        self.variable_dict = {}
 
         # List of clauses which form the CNF
-        self.clauses = 
+        self.clauses = []
 
 
     def load_dimacs(self, file):
         """Encode a dimacs file and add it to the clause list of this CNF_formula"""
         
+        with open(file, "r") as dimacs:
+            
+            # Every line is a clause
+            for line in dimacs:
+
+                # Skip the lines starting with p or c
+                if line[0] == "p" or line[0] == "c":
+                    continue
+
+                # Puts ints in a list. Remove last cause its 0
+                literals = line.split(" ")[:-1]
+                
+                # Turn all literal strings to literal objects 
+                literal_objects = []
+                for literal in literals:
+
+                    literal = int(literal)
+            
+                    # Get (none negated) variable
+                    variable = abs(literal)
+
+                    # If not yet in dict create new dict entry
+                    if variable not in self.variable_dict:
+                        self.variable_dict[str(variable)] = Variable(str(variable))
+                    
+                    # Check if negated
+                    negated = (literal < 0)
+
+                    literal_objects.append(Literal(self.variable_dict[str(variable)], negated))
+
+                # Make new clause and append to CNF clause list
+                clause = Clause(literal_objects)
+                self.clauses.append(clause)
+
+    # Prints all clauses of the CNF
+    def print_clauses(self):
+        for clause in self.clauses:
+            print(clause)  
+
+
+     
+if __name__ == "__main__":
+    
+    CNF = CNF_Formula()
+    CNF.load_dimacs("rules.txt")
+
+    # doesnt fit in console ):
+    # Uncomment to print
+    # CNF.print_clauses()
+    
+    print(f"Variables: {len(CNF.variable_dict)}, Clauses: {len(CNF.clauses)}")
