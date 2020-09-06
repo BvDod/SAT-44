d37a428056a58aad50352d4c0d0cda9d598dff99
Brent <b.dodewaard@gmail.com>
2020-09-06 20:06:18 +0200

    Added SudToCNF to the CNF class and made it work together, you can now load both the rules and sudoku into the same file

--- code/classes/CNF.py
+++ code/classes/CNF.py
@@ -1,5 +1,8 @@
 # File containing classes to represents a CNF formula
 
+import math
+import linecache
+
 class Variable():
     """Represents a variable"""
 
@@ -49,57 +52,89 @@ class CNF_Formula():
         self.clauses = []
 
 
-    def load_dimacs(self, file):
-        """Encode a dimacs file and add it to the clause list of this CNF_formula"""
+    def load_dimacs_string(self, string):
+        """Encode a dimacs string and add it to the clause list of this CNF_formula"""
+           
+        # Every line is a clause
+        for line in string.splitlines():
+
+            # Skip the lines starting with p or c
+            if line[0] == "p" or line[0] == "c":
+                continue
+
+            # Puts ints in a list. Remove last cause its 0
+            literals = line.split(" ")[:-1]
+            
+            # Turn all literal strings to literal objects 
+            literal_objects = []
+            for literal in literals:
+
+                literal = int(literal)
         
-        with open(file, "r") as dimacs:
-            
-            # Every line is a clause
-            for line in dimacs:
+                # Get (none negated) variable
+                variable = abs(literal)
 
-                # Skip the lines starting with p or c
-                if line[0] == "p" or line[0] == "c":
-                    continue
-
-                # Puts ints in a list. Remove last cause its 0
-                literals = line.split(" ")[:-1]
+                # If not yet in dict create new dict entry
+                if variable not in self.variable_dict:
+                    self.variable_dict[str(variable)] = Variable(str(variable))
                 
-                # Turn all literal strings to literal objects 
-                literal_objects = []
-                for literal in literals:
+                # Check if negated
+                negated = (literal < 0)
 
-                    literal = int(literal)
-            
-                    # Get (none negated) variable
-                    variable = abs(literal)
+                literal_objects.append(Literal(self.variable_dict[str(variable)], negated))
 
-                    # If not yet in dict create new dict entry
-                    if variable not in self.variable_dict:
-                        self.variable_dict[str(variable)] = Variable(str(variable))
-                    
-                    # Check if negated
-                    negated = (literal < 0)
+            # Make new clause and append to CNF clause list
+            clause = Clause(literal_objects)
+            self.clauses.append(clause)
+    
+    def load_dimacs_file(self, file):
+        """Accepts a dimac file and turns it into a string, then uses another method to encode it and add it to the clauses"""
+        
+        # Turn file into string
+        with open(file, "r") as dimac:
+            string = dimac.read()
 
-                    literal_objects.append(Literal(self.variable_dict[str(variable)], negated))
+        # Add string to clauses
+        self.load_dimacs_string(string)
 
-                # Make new clause and append to CNF clause list
-                clause = Clause(literal_objects)
-                self.clauses.append(clause)
+    def load_sudoku(self, mfile):
+        # Opens the file, reads the number of sudokus (lines) in it and asks which sudoku you want to solve
+        with open(mfile, "r") as sud_file:
+            nbLines = len(sud_file.readlines())
+        print ("Loading file",mfile)
+        sudNb = input("{} sudokus have been found in this file.\nWhich one should we solve ?\n".format(nbLines))
+        
+        # Opens the file to transform the sudoku line into a string in CNF
+        line = linecache.getline(mfile, int(sudNb))
+        length = math.sqrt(len(line))
+        length = (int(round(length)))
+        sudtorules = []
+        for i in range(length):
+            for j in range(length):
+                n = line[i*length+j]
+                if n!="." :
+                    string = ("{}{}{}".format(i+1,j+1,n))
+                    sudtorules.append(string)
+        sudoku_dimacs = ""
+        for rule in sudtorules:
+            sudoku_dimacs = sudoku_dimacs +("{} 0\n".format(rule))
+        
+        # Use this method to encode the dimac file and add it to the CNF
+        self.load_dimacs_string(sudoku_dimacs)
 
     # Prints all clauses of the CNF
     def print_clauses(self):
         for clause in self.clauses:
             print(clause)  
 
-
      
 if __name__ == "__main__":
     
     CNF = CNF_Formula()
-    CNF.load_dimacs("rules.txt")
-
-    # doesnt fit in console ):
-    # Uncomment to print
-    # CNF.print_clauses()
     
+    CNF.load_dimacs_file("rules.txt")
+    # CNF.print_clauses()
+    print(f"Variables: {len(CNF.variable_dict)}, Clauses: {len(CNF.clauses)}")
+    
+    CNF.load_sudoku("4x4.txt")
     print(f"Variables: {len(CNF.variable_dict)}, Clauses: {len(CNF.clauses)}")
